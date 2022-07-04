4ec0836a82031fc15d890afde85272ec3dc7be93
chamith-onramper <106953947+chamith-onramper@users.noreply.github.com>
2022-07-04 16:10:54 +0530

    React element removed for testing

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -1,33 +1,33 @@
 import { OnramperValidator, isValidData, validate, Gateway } from "../src";
 
-// const validator = new OnramperValidator();
-// console.log(validator.validateField("email", "abcdg@mail.com"));
+const validator = new OnramperValidator();
+console.log(validator.validateField("email", "abcdgmail.com"));
 
-const beData = {
-  cryptocurrencyAddress: "rwt65q8et5q8etqtqwtqetqe",
-  firstName: "James",
-  lastName: "Bond",
-  email: "Jamesgmail.com",
-  phoneCountryCode: 31,
-  //   phoneNumber: 12345678,
-  street: "Test street",
-  town: "Test town",
-  postCode: "123578",
-  country: "nl",
-  state: "undefined",
-  ccNumber: "1234785689975735",
-  ccMonth: "45",
-  ccYear: "2043",
-  ccCVV: "435",
-  termsOfUse: true,
-};
+// const beData = {
+//   cryptocurrencyAddress: "rwt65q8et5q8etqtqwtqetqe",
+//   firstName: "James",
+//   lastName: "Bond",
+//   email: "Jamesgmail.com",
+//   phoneCountryCode: 31,
+//   //   phoneNumber: 12345678,
+//   street: "Test street",
+//   town: "Test town",
+//   postCode: "123578",
+//   country: "nl",
+//   state: "undefined",
+//   ccNumber: "1234785689975735",
+//   ccMonth: "45",
+//   ccYear: "2043",
+//   ccCVV: "435",
+//   termsOfUse: true,
+// };
 
-class DecoratorTest {
-  @validate
-  controllerMethod(@isValidData data: any) {
-    console.log("RUNNING METHOD LOGIC");
-  }
-}
+// class DecoratorTest {
+//   @validate
+//   controllerMethod(@isValidData data: any) {
+//     console.log("RUNNING METHOD LOGIC");
+//   }
+// }
 
-const decorator = new DecoratorTest();
-decorator.controllerMethod(beData);
+// const decorator = new DecoratorTest();
+// decorator.controllerMethod(beData);
